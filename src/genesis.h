153c66a76a5efdbc18ebdb8e98bd37924387990e
global-dev <global@global.com>
2015-05-19 17:49:19 +0100

    Global Release

---
+++ src/genesis.h
@@ -0,0 +1,15 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("000003e0c1e29a31c3332f93ecff92528cf059d5c17a351fb4b8f7e70b8e3932");
+static const uint256 hashGenesisBlockTestNet("000003e0c1e29a31c3332f93ecff92528cf059d5c17a351fb4b8f7e70b8e3932");
+static const uint256 CheckPointBlock1("000000020fd36c6f0d8e635ba14c9cf4678b687b65af8df185e6271c36bf74b9");
+
+// TODO: Move Kernel checksum in here
+
+
+#endif // GENESIS_H
